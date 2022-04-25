3a310e3c5b47209b754c319710e7a22388aeb00a
Wingiu <ganker246@gmail.com>
2022-04-26 01:05:57 +0300

    Remove unused include

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <format>
 
 int main(int argc, char** argv) {
     if (argc != 1) {
         std::cout << "Usage: " << argv[0] << std::endl;
         return 0;
     }
-    
+
     std::cout << "Ultimate Sneed!" << std::endl;
     return 0; 
 } 
